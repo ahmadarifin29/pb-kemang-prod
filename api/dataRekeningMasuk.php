37839d95f6141162ec9a49378e2644da359f92a7
= <=>
2024-02-21 23:58:43 +0700

    perbaikan code

--- api/dataRekeningMasuk.php
+++ api/dataRekeningMasuk.php
@@ -2,6 +2,57 @@
 
 require '../function/functions.php';
 
+[
+    {
+        "id": "1",
+        "kode": "TRX0001",
+        "jumlah": "400.000",
+        "aksi": "masuk",
+        "tanggal": "2019-03-22",
+        "username": "azmirf"
+    },
+    {
+        "id": "2",
+        "kode": "TRX0002",
+        "jumlah": "200.000",
+        "aksi": "masuk",
+        "tanggal": "2019-03-22",
+        "username": "azmirf"
+    },
+    {
+        "id": "3",
+        "kode": "TRX0003",
+        "jumlah": "500.000",
+        "aksi": "masuk",
+        "tanggal": "2019-03-27",
+        "username": "dira"
+    },
+    {
+        "id": "14",
+        "kode": "TRX0005",
+        "jumlah": "300.000",
+        "aksi": "masuk",
+        "tanggal": "2019-04-12",
+        "username": "azmirf"
+    },
+    {
+        "id": "15",
+        "kode": "TRX0006",
+        "jumlah": "100.000",
+        "aksi": "masuk",
+        "tanggal": "2019-04-24",
+        "username": "azmirf"
+    },
+    {
+        "id": "16",
+        "kode": "TRX0007",
+        "jumlah": "200.000",
+        "aksi": "masuk",
+        "tanggal": "2019-04-24",
+        "username": "dina"
+    }
+]
+
 if (empty($_GET['aksi'])) {
     echo '{"status" : "Error", "Message" : "aksi is required!"}';
     exit();
